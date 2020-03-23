0b90456cb136e1d5a6d3990553449e790300aed4
Rick van Essen <vanessen@post.bgu.ac.il>
2020-03-23 14:51:52 +0200

    tested handover with gaze and fixed head

--- intera_sdk/intera_examples/scripts/ReadMe.md
+++
@@ -1,8 +0,0 @@
-Terminal 1:
-roscore
-
-Terminal 2:
-rosrun rosserial_python serial_node.py /dev/ttyACM0
-
-Terminal 3:
-python src/handover_gaze_controller.py
--- tair_files/handover_gaze_controller.py
+++ tair_files/handover_gaze_controller.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python
-"""A program to perform handovers with gaze movements"""
+"""A program to perform handovers with Sawyer Robot"""
 
 import rospy
+import multiprocessing
 from libezgripper import create_connection, Gripper
 import socket
 import time
 from std_msgs.msg import Int32
-from intera_interface import Limb
+from intera_interface import Limb, HeadDisplay, Head
 from intera_motion_msgs.msg import TrajectoryOptions
 from intera_motion_interface import (
     MotionTrajectory,
@@ -14,6 +15,9 @@ from intera_motion_interface import (
     MotionWaypointOptions
 )
 from geometry_msgs.msg import PoseStamped
+import numpy as np
+
+
 # Class to control Sawyer robot
 
 
@@ -22,27 +26,29 @@ class SawyerHandoverController:
         # #########################Fields#####################################
         self.toggle_sensor_reading = 0
         self.range_sensor_reading = 1024
+        self.range_sensor_threshold = 300
         self.connection = create_connection(dev_name='/dev/ttyUSB0', baudrate=57600)
         self.gripper = Gripper(self.connection, 'gripper1', [1])
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.limb = Limb()
         self.traj_options = TrajectoryOptions()
+        self.head_display = HeadDisplay()
+
         self.linear_speed = 0.3
         self.linear_accel = 0.3
-        self.rotational_speed = 1.57/2
-        self.rotational_accel = 1.57/2
+        self.rotational_speed = 1.57 / 2
+        self.rotational_accel = 1.57 / 2
         self.tip_name = "right_hand"
         self.timeout = None
-        self.home_position = [-0.1, 0.3, 0.3, 0.7, 0.7, 0.2, 0]
-        self.waypoint_handover = [-0.1, 0.6, 0.2, 0.6, 0.7, 0.3, -0.2]
+        self.home_position = [-0.1, 0.38, 0.2, 0.5, 0.5, 0.5, -0.5]
+        #self.waypoint_handover = [0, 0.58, 0.2, 0.5, 0.5, 0.5, -0.5]
         self.handover_location = [-0.1, 0.8, 0.2, 0.5, 0.5, 0.5, -0.5]
-
         # ################### Subscribers ####################################
         # Topic for getting toggle sensor reading
-        rospy.Subscriber('/toggle_sensor', Int32, self.toggleSensorCallback)
+        rospy.Subscriber('/toggle_sensor', Int32, self.toggle_sensor_callback)
 
         # Topic for getting range sensor reading
-        rospy.Subscriber('/range_sensor', Int32, self.rangeSensorCallback)
+        rospy.Subscriber('/range_sensor', Int32, self.range_sensor_callback)
 
         # Topic for getting end effector state
         # ################### Publishers ####################################
@@ -52,24 +58,24 @@ class SawyerHandoverController:
         # ################### Services ####################################
 
         # ################## Startup Procedure ############################
-        self.initializeGripper()
-        self.performHandover()
+        self.initialize_gripper()
+        self.perform_handover()
+
     # ################### Action Clients ###################################
-    #def actionClient():
+    # def actionClient():
 
     # ################### Service Clients ##################################
-    #def ServiceClient():
+    # def ServiceClient():
 
     # ################### Callback Functions ###############################
-    def toggleSensorCallback(self, msg):
+    def toggle_sensor_callback(self, msg):
         self.toggle_sensor_reading = msg.data
 
-    def rangeSensorCallback(self, msg):
+    def range_sensor_callback(self, msg):
         self.range_sensor_reading = msg.data
 
-
     # ################### Methods ###########################################
-    def initializeGripper(self):
+    def initialize_gripper(self):
         self.gripper.calibrate()
         self.open_gripper()
 
@@ -77,23 +83,41 @@ class SawyerHandoverController:
         self.gripper.goto_position(80, 100)  # (Position, Effort)
         print "gripper opened"
 
-
     def close_gripper(self):
         self.gripper.goto_position(10, 100)
         print "gripper closed"
 
-    def performHandover(self):
+    def perform_handover(self):
         # Go to home position
-        self.goToCartesianPose(self.waypoint_handover)
-        self.goToCartesianPose(self.home_position)
+        self.go_to_cartesian_pose(self.home_position)
+        # self.go_to_cartesian_pose(self.waypoint_handover)
+        # self.go_to_cartesian_pose(self.home_position)
+        # self.faceGaze()
 
         # Open Gripper
         self.open_gripper()
+        # self.handGaze()
 
         # Go to handover location
-        self.goToCartesianPose(self.waypoint_handover)
-        self.goToCartesianPose(self.handover_location)
+        while self.range_sensor_reading > self.range_sensor_threshold:
+            print(self.range_sensor_reading)
+            # print("not here")
+            rospy.sleep(0.1)
+        else:
+            # workerMovement = multiprocessing.Process(target=self.go_to_cartesian_pose, args = [self.handover_location])
+            # workerMovement.start()
+            # workerEyegaze = multiprocessing.Process(target=self.faceHandTransitionGaze)
+            # print("here")
+            # workerEyegaze.start()
+            # print("also here")
+            # workerMovement.join()
+            # workerEyegaze.join()
+            # print("done")
+            # self.follow_way_points(self.waypoint_handover, self.handover_location)
+            #self.go_to_cartesian_pose(self.waypoint_handover)
+            self.go_to_cartesian_pose(self.handover_location)
 
+            # self.faceHandTransitionGaze()
 
         # Close gripper
         while self.toggle_sensor_reading is 0:
@@ -102,14 +126,17 @@ class SawyerHandoverController:
             self.close_gripper()
 
         # Go to home position
-        self.goToCartesianPose(self.home_position)
+        self.go_to_cartesian_pose(self.home_position)
         rospy.sleep(2.0)
 
-        #Open Gripper
+        # Open Gripper
         self.open_gripper()
 
+    def follow_way_points(self, waypoints1, destination):
+        self.go_to_cartesian_pose(waypoints1)
+        self.go_to_cartesian_pose(destination)
 
-    def goToCartesianPose(self, target_pose):
+    def go_to_cartesian_pose(self, target_pose):
         self.traj_options.interpolation_type = TrajectoryOptions.CARTESIAN
         traj = MotionTrajectory(trajectory_options=self.traj_options, limb=self.limb)
 
@@ -121,9 +148,10 @@ class SawyerHandoverController:
         waypoint = MotionWaypoint(options=wpt_opts.to_msg(), limb=self.limb)
 
         joint_names = self.limb.joint_names()
+        print(joint_names)
         endpoint_state = self.limb.tip_state(self.tip_name)
         if endpoint_state is None:
-            rospy.logerr('Endpoint state not found with tip name %s', pose.tip_name)
+            rospy.logerr('Endpoint state not found with tip name %s', self.tip_name)
             return None
         pose = endpoint_state.pose
 
@@ -136,16 +164,16 @@ class SawyerHandoverController:
             pose.orientation.z = target_pose[5]
             pose.orientation.w = target_pose[6]
 
-        poseStamped = PoseStamped()
-        poseStamped.pose = pose
-
-        # using current joint angles for nullspace bais if not provided
+        pose_stamped = PoseStamped()
+        pose_stamped.pose = pose
+        # using current joint angles for null space basis if not provided
         joint_angles = self.limb.joint_ordered_angles()
-        waypoint.set_cartesian_pose(poseStamped, self.tip_name, joint_angles)
+        waypoint.set_cartesian_pose(pose_stamped, self.tip_name, joint_angles)
 
-        rospy.loginfo('Sending waypoint: \n%s', waypoint.to_string())
+        # rospy.loginfo('Sending waypoint: \n%s', waypoint.to_string())
 
         traj.append_waypoint(waypoint.to_msg())
+        print(target_pose)
 
         result = traj.send_trajectory(timeout=self.timeout)
         if result is None:
